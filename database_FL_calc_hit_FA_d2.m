8678495f4747fd04385ecd872674bf4cf3ebd0e3
junhuang7 <44811884+junhuang7@users.noreply.github.com>
2023-02-02 11:14:49 +0100

    init commit

---
+++ database_FL_calc_hit_FA_d2.m
@@ -0,0 +1,134 @@
+clear TraceTrl_RT lTraceTrl_RT hit miss CR FA EL 
+clear TraceTrl_RTh TraceTrl_RTm TraceTrl_RTc TraceTrl_RTf
+clear lTraceTrl_RTh lTraceTrl_RTm lTraceTrl_RTc lTraceTrl_RTf
+clear meandFF0_RTh meandFF0_RTf lMeanTraceTrl_RTh lMeanTraceTrl_RTf
+%% trial basis definition
+SR=1000; %sampling rate in Hz
+prestart=1;% sec, duration before trial start relative to trigger
+poststart=5;% sec, duriation after trial start relative to trigger
+prestart_RT=3;% sec, duration before trial start relative to trigger
+poststart_RT=3;% sec, duriation after trial start relative to trigger
+basestart=0.001; % sec, baseline start for calculating dFF0_RT relative to trial start trigger
+baseend=1;% sec, baseline end for calculating dFF0_RT relative to trial start trigger
+basestart_RT=0;
+baseend_RT=1;
+timevec=-prestart:1/SR:poststart;
+timevec1 = 0:(1/SR):((length(FL_task.dFF0{j})/SR)-1/SR);
+timevec_RT=-prestart_RT:1/SR:poststart_RT;
+%% calculate signal
+% preallocation of array, which saves 0.2 seconds in one session
+TraceTrl_RT = zeros(length(FL_task.Trial_Onsets{j}),6*SR+1); 
+lTraceTrl_RT = zeros(length(FL_task.Trial_Onsets{j}),6*SR+1);
+
+for i=1:length(FL_task.Trial_Onsets{j})-1;
+    TraceTrl_RT(i,:)=FL_task.dFF0{j}(FL_task.Trial_Onsets{j}(i)*SR-prestart_RT*SR+FL_task.RT{j}(i):FL_task.Trial_Onsets{j}(i)*SR+poststart_RT*SR+FL_task.RT{j}(i));
+end
+
+for i=1:length(FL_task.Trial_Onsets{j})-1;
+    lTraceTrl_RT(i,:)=FL_task.lick{j}(FL_task.Trial_Onsets{j}(i)*SR-prestart_RT*SR+FL_task.RT{j}(i):FL_task.Trial_Onsets{j}(i)*SR+poststart_RT*SR+FL_task.RT{j}(i));
+end
+
+%% define trial type
+
+if size((FL_task.behavior_data{j}),2) ==30;
+    hit=find(FL_task.behavior_data{j}(:,6)==1&FL_task.behavior_data{j}(:,8)==1&FL_task.behavior_data{j}(:,16)==0);
+    miss=find(FL_task.behavior_data{j}(:,6)==1&FL_task.behavior_data{j}(:,8)==0&FL_task.behavior_data{j}(:,16)==0);
+    CR=find(FL_task.behavior_data{j}(:,6)==0&FL_task.behavior_data{j}(:,8)==0&FL_task.behavior_data{j}(:,16)==0);
+    FA=find(FL_task.behavior_data{j}(:,6)==0&FL_task.behavior_data{j}(:,8)==1&FL_task.behavior_data{j}(:,16)==0);
+    EL=find(FL_task.behavior_data{j}(:,16)==1);
+elseif size((FL_task.behavior_data{j}),2) ==18;
+        hit=find(FL_task.behavior_data{j}(:,6)==1&FL_task.behavior_data{j}(:,9)==1);
+        miss=find(FL_task.behavior_data{j}(:,6)==1&FL_task.behavior_data{j}(:,9)==0);
+        CR=find(FL_task.behavior_data{j}(:,6)==0&FL_task.behavior_data{j}(:,9)==0);
+        FA=find(FL_task.behavior_data{j}(:,6)==0&FL_task.behavior_data{j}(:,9)==1);
+        EL=find(isnan(FL_task.behavior_data{j}(:,9)));
+end
+
+hitrate = (length(hit))/(length(hit)+length(miss));
+FArate = length(FA)/(length(FA)+length(CR));
+stimNO = length(hit)+length(miss);
+dprime = norminv(hitrate)-norminv(FArate);
+
+%% calculate FP traces
+TraceTrl_RTh=TraceTrl_RT(hit(hit<length(TraceTrl_RT(:,1))),:);
+TraceTrl_RTm=TraceTrl_RT(miss(miss<length(TraceTrl_RT(:,1))),:);
+TraceTrl_RTc=TraceTrl_RT(CR(CR<length(TraceTrl_RT(:,1))),:);
+TraceTrl_RTf=TraceTrl_RT(FA(FA<length(TraceTrl_RT(:,1))),:);
+
+MeanTraceTrl_RT=mean(TraceTrl_RT,1);
+dFF0_RT=TraceTrl_RT-mean(TraceTrl_RT(:,basestart*SR:baseend*SR),2);
+meandFF0_RT=mean(dFF0_RT,1);
+
+MeanTraceTrl_RTh=mean(TraceTrl_RTh,1);
+dFF0_RTh=TraceTrl_RTh-mean(TraceTrl_RTh(:,basestart*SR:baseend*SR),2);
+meandFF0_RTh=nanmean(dFF0_RTh,1);
+
+MeanTraceTrl_RTm=mean(TraceTrl_RTm,1);
+dFF0_RTm=TraceTrl_RTm-mean(TraceTrl_RTm(:,basestart*SR:baseend*SR),2);
+meandFF0_RTm=nanmean(dFF0_RTm,1);
+
+MeanTraceTrl_RTc=mean(TraceTrl_RTc,1);
+dFF0_RTc=TraceTrl_RTc-mean(TraceTrl_RTc(:,basestart*SR:baseend*SR),2);
+meandFF0_RTc=nanmean(dFF0_RTc,1);
+
+MeanTraceTrl_RTf=mean(TraceTrl_RTf,1);
+dFF0_RTf=TraceTrl_RTf-mean(TraceTrl_RTf(:,basestart*SR:baseend*SR),2);
+meandFF0_RTf=nanmean(dFF0_RTf,1);
+
+%% calculate licking traces
+lTraceTrl_RTh=lTraceTrl_RT(hit(hit<length(TraceTrl_RT(:,1))),:);
+lTraceTrl_RTm=lTraceTrl_RT(miss(miss<length(TraceTrl_RT(:,1))),:);
+lTraceTrl_RTc=lTraceTrl_RT(CR(CR<length(TraceTrl_RT(:,1))),:);
+lTraceTrl_RTf=lTraceTrl_RT(FA(FA<length(TraceTrl_RT(:,1))),:);
+
+lMeanTraceTrl_RT=mean(lTraceTrl_RT,1);
+lMeanTraceTrl_RTh=mean(lTraceTrl_RTh,1);
+lMeanTraceTrl_RTm=mean(lTraceTrl_RTm,1);
+lMeanTraceTrl_RTc=mean(lTraceTrl_RTc,1);
+lMeanTraceTrl_RTf=mean(lTraceTrl_RTf,1);
+
+%% calculate the average of the first 30 trials vs. the last 30 trials
+
+if length(hit)<=30
+    meandFF0_RTh_head=mean(dFF0_RTh,1);
+    meandFF0_RTh_tail=mean(dFF0_RTh,1);
+    lTraceTrl_RTh_head=mean(lTraceTrl_RTh,1);
+    lTraceTrl_RTh_tail=mean(lTraceTrl_RTh,1);
+else
+    meandFF0_RTh_head=mean(dFF0_RTh(1:30,:),1);
+    meandFF0_RTh_tail=mean(dFF0_RTh(end-30:end,:),1);  
+    lTraceTrl_RTh_head=mean(lTraceTrl_RTh(1:30,:),1);
+    lTraceTrl_RTh_tail=mean(lTraceTrl_RTh(end-30:end,:),1);
+end
+
+if length(FA)<=30
+    meandFF0_RTf_head=mean(dFF0_RTf,1);
+    meandFF0_RTf_tail=mean(dFF0_RTf,1);
+    lTraceTrl_RTf_head=mean(lTraceTrl_RTf,1);
+    lTraceTrl_RTf_tail=mean(lTraceTrl_RTf,1);
+else
+    meandFF0_RTf_head=mean(dFF0_RTf(1:30,:),1);
+    meandFF0_RTf_tail=mean(dFF0_RTf(end-30:end,:),1);
+    lTraceTrl_RTf_head=mean(lTraceTrl_RTf(1:30,:),1);
+    lTraceTrl_RTf_tail=mean(lTraceTrl_RTf(end-30:end,:),1);
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ fig_FL_hit_FA_d1_d2.m
@@ -0,0 +1,334 @@
+tic
+clear all
+clc
+
+cd(fileparts(matlab.desktop.editor.getActiveFilename))
+addpath ([(fileparts(matlab.desktop.editor.getActiveFilename)) '\boundedline'])
+addpath ([(fileparts(matlab.desktop.editor.getActiveFilename)) '\Inpaint_nans'])
+load('FL_task_data_struct_single.mat')%load database file
+
+%% figure 3.7 in the thesis 
+
+warning('off','all')
+SR=1000;
+meandFF0_RTh_all=zeros(length(FL_task.mice_NO),6*SR+1);
+meandFF0_RTf_all=zeros(length(FL_task.mice_NO),6*SR+1);
+lMeanTraceTrl_RTh_all=zeros(length(FL_task.mice_NO),6*SR+1);
+lMeanTraceTrl_RTf_all=zeros(length(FL_task.mice_NO),6*SR+1);
+
+meandFF0_RTh_head_all=zeros(length(FL_task.mice_NO),6*SR+1);
+meandFF0_RTf_head_all=zeros(length(FL_task.mice_NO),6*SR+1);
+meandFF0_RTh_tail_all=zeros(length(FL_task.mice_NO),6*SR+1);
+meandFF0_RTf_tail_all=zeros(length(FL_task.mice_NO),6*SR+1);
+lTraceTrl_RTh_head_all=zeros(length(FL_task.mice_NO),6*SR+1);
+lTraceTrl_RTf_head_all=zeros(length(FL_task.mice_NO),6*SR+1);
+lTraceTrl_RTh_tail_all=zeros(length(FL_task.mice_NO),6*SR+1);
+lTraceTrl_RTf_tail_all=zeros(length(FL_task.mice_NO),6*SR+1);
+
+for j = 1:length(FL_task.mice_NO)
+    database_FL_calc_hit_FA_d2
+    meandFF0_RTh_all(j,:)=meandFF0_RTh;
+    meandFF0_RTf_all(j,:)=meandFF0_RTf;
+    lMeanTraceTrl_RTh_all(j,:)=lMeanTraceTrl_RTh;
+    lMeanTraceTrl_RTf_all(j,:)=lMeanTraceTrl_RTf;
+    meandFF0_RTh_head_all(j,:)=meandFF0_RTh_head;
+    meandFF0_RTh_tail_all(j,:)=meandFF0_RTh_tail;
+    meandFF0_RTf_head_all(j,:)=meandFF0_RTf_head;
+    meandFF0_RTf_tail_all(j,:)=meandFF0_RTf_tail;
+    lTraceTrl_RTh_head_all(j,:)=lTraceTrl_RTh_head;
+    lTraceTrl_RTh_tail_all(j,:)=lTraceTrl_RTh_tail;
+    lTraceTrl_RTf_head_all(j,:)=lTraceTrl_RTf_head;
+    lTraceTrl_RTf_tail_all(j,:)=lTraceTrl_RTf_tail;
+end
+
+%dLight trace calculations
+meandFF0_RTh_d1=mean(meandFF0_RTh_all(1:2:length(FL_task.mice_NO),:),1);
+stddFF0_RTh_d1=std(meandFF0_RTh_all(1:2:length(FL_task.mice_NO),:),1,1);
+meandFF0_RTf_d1=mean(meandFF0_RTf_all(1:2:length(FL_task.mice_NO),:),1);
+stddFF0_RTf_d1=std(meandFF0_RTf_all(1:2:length(FL_task.mice_NO),:),1,1);
+
+meandFF0_RTh_d2=mean(meandFF0_RTh_all(2:2:length(FL_task.mice_NO),:),1);
+stddFF0_RTh_d2=std(meandFF0_RTh_all(2:2:length(FL_task.mice_NO),:),1,1);
+meandFF0_RTf_d2=mean(meandFF0_RTf_all(2:2:length(FL_task.mice_NO),:),1);
+stddFF0_RTf_d2=std(meandFF0_RTf_all(2:2:length(FL_task.mice_NO),:),1,1);
+
+%licking trace calculations
+lMeanTraceTrl_RTh_d1=mean(lMeanTraceTrl_RTh_all(1:2:length(FL_task.mice_NO),:),1);
+stdlTraceTrl_RTh_d1=std(lMeanTraceTrl_RTh_all(1:2:length(FL_task.mice_NO),:),1,1);
+lMeanTraceTrl_RTf_d1=mean(lMeanTraceTrl_RTf_all(1:2:length(FL_task.mice_NO),:),1);
+stdlTraceTrl_RTf_d1=std(lMeanTraceTrl_RTf_all(1:2:length(FL_task.mice_NO),:),1,1);
+
+lMeanTraceTrl_RTh_d2=mean(lMeanTraceTrl_RTh_all(2:2:length(FL_task.mice_NO),:),1);
+stdlTraceTrl_RTh_d2=std(lMeanTraceTrl_RTh_all(2:2:length(FL_task.mice_NO),:),1,1);
+lMeanTraceTrl_RTf_d2=mean(lMeanTraceTrl_RTf_all(2:2:length(FL_task.mice_NO),:),1);
+stdlTraceTrl_RTf_d2=std(lMeanTraceTrl_RTf_all(2:2:length(FL_task.mice_NO),:),1,1);
+
+
+%% fig3_7 plotting of R+ vs. R- trials in free-licking day 2
+
+figure(1)
+boundedline(timevec_RT,meandFF0_RTh_d2,stddFF0_RTh_d2./sqrt(length(FL_task.mice_NO)/2),'b')
+hold on
+boundedline(timevec_RT,meandFF0_RTf_d2,stddFF0_RTf_d2./sqrt(length(FL_task.mice_NO)/2),'r')
+hold on
+h1=plot(timevec_RT,meandFF0_RTh_d2,'b-','LineWidth',2)
+hold on
+h2=plot(timevec_RT,meandFF0_RTf_d2,'r-','LineWidth',2)
+lgd = legend;
+lgd.NumColumns = 1;
+hold on
+h3=line([0,0],[-0.01,0.01],'color','black','linewidth',2,'DisplayName','Lick On');
+set(gca,'xtick',(-3:3))
+set(gca,'xticklabel',num2cell(-3:3),'FontSize',20)
+set(gca,'box','off')
+legend boxoff 
+ylim([-0.0149 0.02]) 
+xx=legend([h1 h2 h3], 'R+', 'R-','Lick On');
+set(xx,'FontSize',12);
+xlabel('time (sec)','FontSize',20)
+ylabel('\DeltaF/F_0','FontSize',20)
+title(['Day2 (n = 41 mice)'],'FontSize',20)
+h=figure(1)
+set(h,'PaperSize',[8 6]); %set the paper size to what you want  
+print('fig3_7_FL_hit_FA_d2_dLight', '-dpdf', '-r300'); %<-Save as PNG with 300 DPI
+% close
+
+figure(2)
+boundedline(timevec_RT,lMeanTraceTrl_RTh_d2,stdlTraceTrl_RTh_d2./sqrt(length(FL_task.mice_NO)/2),'b')
+hold on
+boundedline(timevec_RT,lMeanTraceTrl_RTf_d2,stdlTraceTrl_RTf_d2./sqrt(length(FL_task.mice_NO)/2),'r')
+hold on
+h1=plot(timevec_RT,lMeanTraceTrl_RTh_d2,'b-','LineWidth',2)
+hold on
+h2=plot(timevec_RT,lMeanTraceTrl_RTf_d2,'r-','LineWidth',2)
+lgd = legend;
+lgd.NumColumns = 1;
+hold on
+h3=line([0,0],[-0.01,0.1],'color','black','linewidth',2,'DisplayName','Lick On');
+set(gca,'xtick',(-3:3))
+set(gca,'xticklabel',num2cell(-3:3),'FontSize',20)
+set(gca,'box','off')
+legend boxoff 
+ylim([-0.01 0.2]) 
+xx=legend([h1 h2 h3], 'R+', 'R-','Lick On');
+set(xx,'FontSize',12);
+xlabel('time (sec)','FontSize',20)
+ylabel('Lick (A.U.)','FontSize',20)
+title(['Day2 (n = 41 mice)'],'FontSize',20)
+h=figure(2)
+set(h,'PaperSize',[8 6]); %set the paper size to what you want  
+print('fig3_7_FL_hit_FA_d2_lick', '-dpdf', '-r300'); %<-Save as PNG with 300 DPI
+% close
+
+%% fig3_9 sucrose reward vs. water reward
+
+% dLight trace calculations
+% Since the first 33 animals are sucrose-rewarded, and the rest 8 animals
+% are water rewarded, so we are going to divide the two groups based on the
+% index of the array.
+
+meandFF0_RTh_d2_suc=mean(meandFF0_RTh_all(2:2:33*2,:),1);
+stddFF0_RTh_d2_suc=std(meandFF0_RTh_all(2:2:33*2,:),1,1);
+meandFF0_RTf_d2_suc=mean(meandFF0_RTf_all(2:2:33*2,:),1);
+stddFF0_RTf_d2_suc=std(meandFF0_RTf_all(2:2:33*2,:),1,1);
+
+meandFF0_RTh_d2_water=mean(meandFF0_RTh_all(34*2:2:end,:),1);
+stddFF0_RTh_d2_water=std(meandFF0_RTh_all(34*2:2:end,:),1,1);
+meandFF0_RTf_d2_water=mean(meandFF0_RTf_all(34*2:2:end,:),1);
+stddFF0_RTf_d2_water=std(meandFF0_RTf_all(34*2:2:end,:),1,1);
+
+figure(3)
+boundedline(timevec_RT,meandFF0_RTh_d2_suc,stddFF0_RTh_d2_suc./sqrt(33),'b')
+hold on
+boundedline(timevec_RT,meandFF0_RTf_d2_suc,stddFF0_RTf_d2_suc./sqrt(33),'r')
+hold on
+h1=plot(timevec_RT,meandFF0_RTh_d2_suc,'b-','LineWidth',2)
+hold on
+h2=plot(timevec_RT,meandFF0_RTf_d2_suc,'r-','LineWidth',2)
+lgd = legend;
+lgd.NumColumns = 1;
+hold on
+h3=line([0,0],[-0.01,0.01],'color','black','linewidth',2,'DisplayName','Lick On');
+set(gca,'xtick',(-3:3))
+set(gca,'xticklabel',num2cell(-3:3),'FontSize',20)
+set(gca,'box','off')
+legend boxoff 
+ylim([-0.0149 0.02]) 
+xx=legend([h1 h2 h3], 'R+', 'R-','Lick On');
+set(xx,'FontSize',12);
+xlabel('time (sec)','FontSize',20)
+ylabel('\DeltaF/F_0','FontSize',20)
+title(['Sucrose (n=33)'],'FontSize',20)
+h=figure(3)
+set(h,'PaperSize',[8 6]); %set the paper size to what you want  
+print('fig3_9_FL_hit_FA_d2_dLight_sucrose', '-dpdf', '-r300'); %<-Save as PNG with 300 DPI
+
+figure(4)
+boundedline(timevec_RT,meandFF0_RTh_d2_water,stddFF0_RTh_d2_water./sqrt(8),'b')
+hold on
+boundedline(timevec_RT,meandFF0_RTf_d2_water,stddFF0_RTf_d2_water./sqrt(8),'r')
+hold on
+h1=plot(timevec_RT,meandFF0_RTh_d2_water,'b-','LineWidth',2)
+hold on
+h2=plot(timevec_RT,meandFF0_RTf_d2_water,'r-','LineWidth',2)
+lgd = legend;
+lgd.NumColumns = 1;
+hold on
+h3=line([0,0],[-0.01,0.01],'color','black','linewidth',2,'DisplayName','Lick On');
+set(gca,'xtick',(-3:3))
+set(gca,'xticklabel',num2cell(-3:3),'FontSize',20)
+set(gca,'box','off')
+legend boxoff 
+ylim([-0.0149 0.02]) 
+xx=legend([h1 h2 h3], 'R+', 'R-','Lick On');
+set(xx,'FontSize',12);
+xlabel('time (sec)','FontSize',20)
+ylabel('\DeltaF/F_0','FontSize',20)
+title(['Water (n=8)'],'FontSize',20)
+h=figure(4)
+set(h,'PaperSize',[8 6]); %set the paper size to what you want  
+print('fig3_9_FL_hit_FA_d2_dLight_water', '-dpdf', '-r300'); %<-Save as PNG with 300 DPI
+
+%% fig3_11 first 30 vs. last 30 trials
+
+
+meandFF0_RTh_d2_head=mean(meandFF0_RTh_head_all(2:2:end,:),1);
+stddFF0_RTh_d2_head=std(meandFF0_RTh_head_all(2:2:end,:),1,1);
+meandFF0_RTf_d2_head=mean(meandFF0_RTf_head_all(2:2:end,:),1);
+stddFF0_RTf_d2_head=std(meandFF0_RTf_head_all(2:2:end,:),1,1);
+
+meandFF0_RTh_d2_tail=mean(meandFF0_RTh_tail_all(2:2:end,:),1);
+stddFF0_RTh_d2_tail=std(meandFF0_RTh_tail_all(2:2:end,:),1,1);
+meandFF0_RTf_d2_tail=mean(meandFF0_RTf_tail_all(2:2:end,:),1);
+stddFF0_RTf_d2_tail=std(meandFF0_RTf_tail_all(2:2:end,:),1,1);
+
+lTraceTrl_RTh_d2_head=mean(lTraceTrl_RTh_head_all(2:2:length(FL_task.mice_NO),:),1);
+stdlTraceTrl_RTh_d2_head=std(lTraceTrl_RTh_head_all(2:2:length(FL_task.mice_NO),:),1,1);
+lTraceTrl_RTf_d2_head=mean(lTraceTrl_RTf_head_all(2:2:length(FL_task.mice_NO),:),1);
+stdlTraceTrl_RTf_d2_head=std(lTraceTrl_RTf_head_all(2:2:length(FL_task.mice_NO),:),1,1);
+
+lTraceTrl_RTh_d2_tail=mean(lTraceTrl_RTh_tail_all(2:2:length(FL_task.mice_NO),:),1);
+stdlTraceTrl_RTh_d2_tail=std(lTraceTrl_RTh_tail_all(2:2:length(FL_task.mice_NO),:),1,1);
+lTraceTrl_RTf_d2_tail=mean(lTraceTrl_RTf_tail_all(2:2:length(FL_task.mice_NO),:),1);
+stdlTraceTrl_RTf_d2_tail=std(lTraceTrl_RTf_tail_all(2:2:length(FL_task.mice_NO),:),1,1);
+
+
+figure(5)
+boundedline(timevec_RT,meandFF0_RTh_d2_head,stddFF0_RTh_d2_head./sqrt(length(FL_task.mice_NO)/2),'b')
+hold on
+boundedline(timevec_RT,meandFF0_RTh_d2_tail,stddFF0_RTh_d2_tail./sqrt(length(FL_task.mice_NO)/2),'r')
+hold on
+h1=plot(timevec_RT,meandFF0_RTh_d2_head,'b-','LineWidth',2)
+hold on
+h2=plot(timevec_RT,meandFF0_RTh_d2_tail,'r-','LineWidth',2)
+lgd = legend;
+lgd.NumColumns = 1;
+hold on
+h3=line([0,0],[-0.01,0.01],'color','black','linewidth',2,'DisplayName','Lick On');
+set(gca,'xtick',(-3:3))
+set(gca,'xticklabel',num2cell(-3:3),'FontSize',20)
+set(gca,'box','off')
+legend boxoff 
+ylim([-0.0149 0.03]) 
+xx=legend([h1 h2 h3], 'First 30 trials', 'Last 30 trials','Lick On');
+set(xx,'FontSize',12);
+xlabel('time (sec)','FontSize',20)
+ylabel('\DeltaF/F_0','FontSize',20)
+title(['R+ trials (n = 41 mice)'],'FontSize',20)
+h=figure(5)
+set(h,'PaperSize',[8 6]); %set the paper size to what you want  
+print('fig3_11_FL_hit_FA_d2_dLight_R+30', '-dpdf', '-r300'); %<-Save as PNG with 300 DPI
+
+figure(6)
+boundedline(timevec_RT,meandFF0_RTf_d2_head,stddFF0_RTf_d2_head./sqrt(length(FL_task.mice_NO)/2),'b')
+hold on
+boundedline(timevec_RT,meandFF0_RTf_d2_tail,stddFF0_RTf_d2_tail./sqrt(length(FL_task.mice_NO)/2),'r')
+hold on
+h1=plot(timevec_RT,meandFF0_RTf_d2_head,'b-','LineWidth',2)
+hold on
+h2=plot(timevec_RT,meandFF0_RTf_d2_tail,'r-','LineWidth',2)
+lgd = legend;
+lgd.NumColumns = 1;
+hold on
+h3=line([0,0],[-0.01,0.01],'color','black','linewidth',2,'DisplayName','Lick On');
+set(gca,'xtick',(-3:3))
+set(gca,'xticklabel',num2cell(-3:3),'FontSize',20)
+set(gca,'box','off')
+legend boxoff 
+ylim([-0.0149 0.03]) 
+xx=legend([h1 h2 h3], 'First 30 trials', 'Last 30 trials','Lick On');
+set(xx,'FontSize',12);
+xlabel('time (sec)','FontSize',20)
+ylabel('\DeltaF/F_0','FontSize',20)
+title(['R- trials (n = 41 mice)'],'FontSize',20)
+h=figure(6)
+set(h,'PaperSize',[8 6]); %set the paper size to what you want  
+print('fig3_11_FL_hit_FA_d2_dLight_R-30', '-dpdf', '-r300'); %<-Save as PNG with 300 DPI
+
+
+
+figure(7)
+boundedline(timevec_RT,lTraceTrl_RTh_d2_head,stdlTraceTrl_RTh_d2_head./sqrt(length(FL_task.mice_NO)/2),'b')
+hold on
+boundedline(timevec_RT,lTraceTrl_RTh_d2_tail,stdlTraceTrl_RTh_d2_tail./sqrt(length(FL_task.mice_NO)/2),'r')
+hold on
+h1=plot(timevec_RT,lTraceTrl_RTh_d2_head,'b-','LineWidth',2)
+hold on
+h2=plot(timevec_RT,lTraceTrl_RTh_d2_tail,'r-','LineWidth',2)
+lgd = legend;
+lgd.NumColumns = 1;
+hold on
+h3=line([0,0],[-0.01,0.1],'color','black','linewidth',2,'DisplayName','Lick On');
+set(gca,'xtick',(-3:3))
+set(gca,'xticklabel',num2cell(-3:3),'FontSize',20)
+set(gca,'box','off')
+legend boxoff 
+ylim([-0.0149 0.3]) 
+xx=legend([h1 h2 h3], 'First 30 trials', 'Last 30 trials','Lick On');
+set(xx,'FontSize',12);
+xlabel('time (sec)','FontSize',20)
+ylabel('Lick (A.U.)','FontSize',20)
+title(['R+ trials (n = 41 mice)'],'FontSize',20)
+h=figure(7)
+set(h,'PaperSize',[8 6]); %set the paper size to what you want  
+print('fig3_11_FL_hit_FA_d2_lick_R+30', '-dpdf', '-r300'); %<-Save as PNG with 300 DPI
+
+figure(8)
+boundedline(timevec_RT,lTraceTrl_RTf_d2_head,stdlTraceTrl_RTf_d2_head./sqrt(length(FL_task.mice_NO)/2),'b')
+hold on
+boundedline(timevec_RT,lTraceTrl_RTf_d2_tail,stdlTraceTrl_RTf_d2_tail./sqrt(length(FL_task.mice_NO)/2),'r')
+hold on
+h1=plot(timevec_RT,lTraceTrl_RTf_d2_head,'b-','LineWidth',2)
+hold on
+h2=plot(timevec_RT,lTraceTrl_RTf_d2_tail,'r-','LineWidth',2)
+lgd = legend;
+lgd.NumColumns = 1;
+hold on
+h3=line([0,0],[-0.01,0.1],'color','black','linewidth',2,'DisplayName','Lick On');
+set(gca,'xtick',(-3:3))
+set(gca,'xticklabel',num2cell(-3:3),'FontSize',20)
+set(gca,'box','off')
+legend boxoff 
+ylim([-0.0149 0.3]) 
+xx=legend([h1 h2 h3], 'First 30 trials', 'Last 30 trials','Lick On');
+set(xx,'FontSize',12);
+xlabel('time (sec)','FontSize',20)
+ylabel('Lick (A.U.)','FontSize',20)
+title(['R- trials (n = 41 mice)'],'FontSize',20)
+h=figure(8)
+set(h,'PaperSize',[8 6]); %set the paper size to what you want  
+print('fig3_11_FL_hit_FA_d2_lick_R-30', '-dpdf', '-r300'); %<-Save as PNG with 300 DPI
+
+
+
+
+
+
+
+
+
+
+
+
+
+toc
